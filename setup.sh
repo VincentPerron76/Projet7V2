6792ef8c208b94597e5e3a654eb0f8af1aab5043
VincentPERRON76 <svennn@hotmail.fr>
2024-12-11 00:18:23 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Mise à jour des paquets
+sudo apt-get update
+
+# Installation de python3-distutils
+sudo apt-get install -y python3-distutils
+
+# Autres dépendances système que vous pourriez avoir besoin d'ajouter
+# sudo apt-get install -y <autre_paquet>
