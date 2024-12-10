9e34c4b5e97495adc93560665980aaf40aa44d55
VincentPERRON76 <svennn@hotmail.fr>
2024-12-11 00:26:56 +0100

    setupools

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 sudo apt-get update
 
 # Installation de python3-distutils
-sudo apt-get install -y python3-distutils
+sudo apt-get install -y python3-venv
+pip install setuptools
 
 # Autres dépendances système que vous pourriez avoir besoin d'ajouter
 # sudo apt-get install -y <autre_paquet>
