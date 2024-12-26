238beff05ccc7ca96fd305ecd2249308d59dc032
VincentPERRON76 <svennn@hotmail.fr>
2024-12-26 17:50:06 +0100

    modif api

--- api.py
+++ api.py
@@ -64,5 +64,8 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 # Lancer l'application Flask sur le port 5001
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001)) 
+    app.run(host='0.0.0.0',debug=True, port=5001)
